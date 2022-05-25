db252fdba236f0014632301ece6927229f5276a4
Cedric Travelletti <cedrictravelletti@gmail.com>
2022-05-25 15:26:37 +0200

    BUGFIX

--- scripts/repackage_grace.py
+++ scripts/repackage_grace.py
@@ -17,21 +17,29 @@ col_names = ["MJD", "frac. of a day", "GPS range AB [m]",
         "Kband range rate [m/s]", "O-C range rate [m/s]", 
         "Latitude [°]", "Longitude [°]", "Arg. of lat. [°]", "beta [°]"]
 
-# Read and concatenate all dataframes.
-dfs = []
-for path in glob.glob(os.path.join(data_folder, "dataset_residuals_operationalSolution/*/*.RES")):
-        dfs.append(pd.read_csv(path, sep="\s+", skiprows=12, names=col_names))
 
-df = pd.concat(dfs)
+def main():
+    # Read and concatenate all dataframes.
+    dfs = []
+    for path in glob.glob(os.path.join(data_folder, "dataset_residuals_operationalSolution/*/*.RES")):
+            dfs.append(pd.read_csv(path, sep="\s+", skiprows=12, names=col_names))
+    
+    df = pd.concat(dfs)
+    
+    # Convert times from modified Julian to datetime.
+    df['time'] = Time(df["MJD"] + df["frac. of a day"], format="mjd")
+    df['time'] = df['time'].apply(lambda x: x.to_value('datetime'))
+    df = df.set_index("time")
+    
+    # Use xarray to store in Zarr.
+    # Also chunk data so can load partially.
+    ds = xr.Dataset.from_dataframe(df)
+    ds = ds.chunk(25000)
+    
+    # Save to zarr format.
+    ds.to_zarr(os.path.join(data_folder, "./GRACE_merged_2019.zarr"), mode='w')
+    ds.close()
 
-# Convert times from modified Julian to datetime.
-df['time'] = Time(df["MJD"] + df["frac. of a day"], format="mjd")
-df.set_index("time")
 
-# Use xarray to store in Zarr.
-# Also chunk data so can load partially.
-ds = xr.Dataset.from_dataframe(df)
-ds = ds.chunk(25000)
-
-# Save to zarr format.
-ds.to_zarr(os.path.join(data_folder, "./GRACE_merged_2019.zarr"), mode='w')
+if __name__ == "__main__":
+    main()
